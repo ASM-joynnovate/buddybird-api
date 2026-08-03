6c8b6770250ad5fee58948a301bb77161d3c47c9
PRESSO <caff1nepill@gmail.com>
2026-08-03 15:38:45 +0900

    feat(word): change word route name

--- app/word/presentation/rest/__init__.py
+++ app/word/presentation/rest/__init__.py
@@ -4,4 +4,4 @@ from .v1.router import router as word_v1_router
 
 router = APIRouter()
 
-router.include_router(word_v1_router, prefix="/v1/word", tags=["단어"])
+router.include_router(word_v1_router, prefix="/v1/words", tags=["단어"])
